41b2fce4bedcf178f4e5241073aa7bbdb7bfcb28
aokmen <abdullah.okmen.11@gmail.com>
2023-08-26 08:09:19 +0200

    defaultImage_added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,7 +9,7 @@ import { AuthContex } from "../context/AuthContext";
 
 const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
   const IMG_API = "https://image.tmdb.org/t/p/w1280";
-
+const defaultImage = "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
   const { currentUser } = useContext(AuthContex);
   const navigate = useNavigate();
@@ -33,7 +33,7 @@ const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
     >
       <img
         loading="lazy"
-        src={IMG_API + poster_path}
+        src={IMG_API + poster_path || defaultImage}
         alt="movie-card"
       />
       <div className="flex align-baseline justify-between p-1 text-white">
