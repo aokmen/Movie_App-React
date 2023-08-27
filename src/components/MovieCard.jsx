adfb486e33a21c6d91b5a6fa8dff179219fdab86
aokmen <abdullah.okmen.11@gmail.com>
2023-08-27 20:34:22 +0200

    moviecontext_fixed

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,20 +1,19 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { toastWarnNotify } from "../helpers/TostNotify";
+
 import { AuthContex } from "../context/AuthContext";
+import { toastWarnNotify } from "../helpers/TostNotify";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
-const defaultImage = "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
-
+const defaultImage =
+  "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
 const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
-
-
   const { currentUser } = useContext(AuthContex);
   const navigate = useNavigate();
 
-  const getVoteClass = (vote) => { 
+  const getVoteClass = (vote) => {
     if (vote >= 8) {
       return "green";
     } else if (vote >= 6) {
@@ -33,7 +32,6 @@ const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
     >
       <img
         loading="lazy"
-
         src={poster_path ? IMG_API + poster_path : defaultImage}
         alt="movie-card"
       />
