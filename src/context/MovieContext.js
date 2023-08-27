64c3b947041d502dc04dd11cd0a66f6226dfd879
aokmen <abdullah.okmen.11@gmail.com>
2023-08-27 18:30:45 +0200

    mock_key_added

--- src/context/MovieContext.js
+++ src/context/MovieContext.js
@@ -2,7 +2,7 @@ import React, { createContext, useEffect, useState } from 'react'
 import axios from "axios"
 
 export const MovieContext = createContext()
-const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const API_KEY = process.env.REACT_APP_TMDB_KEY  || 'mock_key';
 const FULL_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 
 
