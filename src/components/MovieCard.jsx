8c710b84e43c111d7ac189b24ebdd7bff20e6355
aokmen <abdullah.okmen.11@gmail.com>
2023-08-27 17:02:33 +0200

    img_added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,11 +5,7 @@ import { toastWarnNotify } from "../helpers/TostNotify";
 import { AuthContex } from "../context/AuthContext";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
-<<<<<<< HEAD
 const defaultImage = "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
-=======
-//const defaultImage = "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
->>>>>>> 1486fc05cb1faf1624f0562e372861adbccd9f94
 
 
 const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
@@ -37,13 +33,8 @@ const MovieCard = ({ poster_path, title, overview, vote_average, id }) => {
     >
       <img
         loading="lazy"
-<<<<<<< HEAD
 
         src={IMG_API + poster_path || defaultImage}
-=======
-        src={IMG_API + poster_path}
-        //src={IMG_API + poster_path || defaultImage}
->>>>>>> 1486fc05cb1faf1624f0562e372861adbccd9f94
         alt="movie-card"
       />
       <div className="flex align-baseline justify-between p-1 text-white">
